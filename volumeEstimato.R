c6ee165bb2dc332d5e8f33f474862af0671f124f
Sirui Zhao <122402777+siruiZHAO@users.noreply.github.com>
2023-01-11 18:39:42 -0500

    Add files via upload

---
+++ volumeEstimato.R
@@ -0,0 +1,28 @@
+##Data Import and library
+library(dplyr)
+setwd("/Users/siruizhao/BIOL432_Assignment1")
+getwd()
+mydata <- read.csv("measurements.csv")
+
+#check data
+head(mydata)
+tail(mydata)
+class(mydata)
+str(mydata)
+
+#create new variable called limb_volume
+#in this assignment, we assume limb is cylinder and can be calculated 
+#through v = pi * r^2 * h
+mydata <- mydata %>%
+  mutate(limb_volume = pi * ((mydata$Limb_width/2)^2) * mydata$Limb_length)
+#check
+head(mydata)
+
+# write as csv file
+write.csv(mydata, "measurements.csv")
+
+
+
+
+
+
